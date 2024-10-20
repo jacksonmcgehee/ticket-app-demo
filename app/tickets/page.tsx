2d59b1751f5cf9d8744296069d4f9957572de2f6
Jackson McGehee <jacksonmcgehee@gmail.com>
2024-10-20 15:52:40 -0400

    Added Prisma client, seeded local DB, pulled records to test

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,6 +1,10 @@
+import prisma from '@/prisma/db';
 
+const Tickets = async () => {
 
-const Tickets = () => {
+    const tickets = await prisma.ticket.findMany();
+
+    console.log(tickets);
     return (
         <div>
         <h1>Tickets</h1>
