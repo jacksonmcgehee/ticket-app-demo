457117279d89c901ad5e57aed853e77cb81217f4
Jackson McGehee <jacksonmcgehee@gmail.com>
2024-10-27 11:09:41 -0400

    Added update UI

--- components/TicketForm.tsx
+++ components/TicketForm.tsx
@@ -12,10 +12,15 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from ".
 import { Button } from "./ui/button";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { Ticket } from "@prisma/client";
 
 type TicketFormData = z.infer<typeof ticketSchema>
 
-const TicketForm = () => {
+interface Props {
+    ticket?: Ticket;
+}
+
+const TicketForm = ({ ticket }: Props) => {
 
     const [isSubmitting, setIsSubmitting] = useState(false);
     const [error, setError] = useState('');
@@ -30,7 +35,11 @@ const TicketForm = () => {
             setIsSubmitting(true);
             setError('');
 
-            await axios.post('/api/tickets', values);
+            if (ticket) {
+                await axios.patch(`/api/tickets/${ticket.id}`, values);
+            } else {
+                await axios.post('/api/tickets', values);
+            }
             setIsSubmitting(false);
 
             router.push("/tickets");
@@ -46,54 +55,70 @@ const TicketForm = () => {
         <div className="rounded-md border w-full p-4">
             <Form {...form}>
                 <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
-                    <FormField control={form.control} name="title" render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Ticket Title</FormLabel>
-                            <FormControl>
-                                <Input placeholder="Ticket Title..." {...field}/>
-                            </FormControl>
-                        </FormItem>
-                    )} />
-                    <Controller name="description" control={form.control} render={({ field }) => (
+                    <FormField 
+                        control={form.control}
+                        name="title"
+                        defaultValue={ticket?.title}
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Ticket Title</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="Ticket Title..." {...field}/>
+                                </FormControl>
+                            </FormItem>
+                        )} />
+                    <Controller
+                        name="description"
+                        control={form.control}
+                        defaultValue={ticket?.description}
+                        render={({ field }) => (
                         <SimpleMDE placeholder="Description" {...field}/>
                     )} />
                     <div className="flex w-full space-x-4">
-                        <FormField control={form.control} name="status" render={({ field}) => (
-                            <FormItem>
-                                <FormLabel>Status</FormLabel>
-                                <Select onValueChange={field.onChange} defaultValue={field.value} >
-                                    <FormControl>
-                                        <SelectTrigger>
-                                            <SelectValue placeholder="Status..." />
-                                        </SelectTrigger>
-                                    </FormControl>
-                                    <SelectContent>
-                                        <SelectItem value="OPEN">Open</SelectItem>
-                                        <SelectItem value="STARTED">Started</SelectItem>
-                                        <SelectItem value="CLOSED">Closed</SelectItem>
-                                    </SelectContent>
-                                </Select>
-                            </FormItem>
-                        )} />
-                        <FormField control={form.control} name="priority" render={({ field}) => (
-                            <FormItem>
-                                <FormLabel>Priority</FormLabel>
-                                <Select onValueChange={field.onChange} defaultValue={field.value} >
-                                    <FormControl>
-                                        <SelectTrigger>
-                                            <SelectValue placeholder="Priority..." />
-                                        </SelectTrigger>
-                                    </FormControl>
-                                    <SelectContent>
-                                        <SelectItem value="LOW">Low</SelectItem>
-                                        <SelectItem value="MEDIUM">Medium</SelectItem>
-                                        <SelectItem value="HIGH">High</SelectItem>
-                                    </SelectContent>
-                                </Select>
-                            </FormItem>
-                        )} />
+                        <FormField
+                            control={form.control}
+                            name="status"
+                            defaultValue={ticket?.status}
+                            render={({ field}) => (
+                                <FormItem>
+                                    <FormLabel>Status</FormLabel>
+                                    <Select onValueChange={field.onChange} defaultValue={field.value} >
+                                        <FormControl>
+                                            <SelectTrigger>
+                                                <SelectValue placeholder="Status..." defaultValue={ticket?.status}/>
+                                            </SelectTrigger>
+                                        </FormControl>
+                                        <SelectContent>
+                                            <SelectItem value="OPEN">Open</SelectItem>
+                                            <SelectItem value="STARTED">Started</SelectItem>
+                                            <SelectItem value="CLOSED">Closed</SelectItem>
+                                        </SelectContent>
+                                    </Select>
+                                </FormItem>
+                            )} />
+                        <FormField
+                            control={form.control}
+                            name="priority"
+                            defaultValue={ticket?.priority}
+                            render={({ field}) => (
+                                <FormItem>
+                                    <FormLabel>Priority</FormLabel>
+                                    <Select onValueChange={field.onChange} defaultValue={field.value} >
+                                        <FormControl>
+                                            <SelectTrigger>
+                                                <SelectValue placeholder="Priority..." defaultValue={ticket?.priority} />
+                                            </SelectTrigger>
+                                        </FormControl>
+                                        <SelectContent>
+                                            <SelectItem value="LOW">Low</SelectItem>
+                                            <SelectItem value="MEDIUM">Medium</SelectItem>
+                                            <SelectItem value="HIGH">High</SelectItem>
+                                        </SelectContent>
+                                    </Select>
+                                </FormItem>
+                            )} />
                     </div> 
-                    <Button type="submit" disabled={isSubmitting}>Create Ticket</Button>
+                    <Button type="submit" disabled={isSubmitting}>{ticket ? "Update Ticket" : "Create Ticket"}</Button>
                 </form>
             </Form>
         </div>
